60dd8d8b612dc160749b19fceab6a83d1e20c1bc
jesuscast <jesus.cast.sosa@gmail.com>
2016-08-28 10:31:18 -0700

    sad'

--- index.js
+++ index.js
@@ -459,6 +459,7 @@ function send_photo_to_user(user){
 
 app.post('/gifwar/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
+    console.log(req.body.entry[0].messaging)
     for (let i = 0; i < messaging_events.length; i++) {
       let event = req.body.entry[0].messaging[i]
       let sender = event.sender.id
